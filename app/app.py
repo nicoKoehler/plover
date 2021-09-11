edbea5442a70edfc6b44de43a0fb74df13d10917
nicoKoehler <nk.logins@pm.me>
2021-09-11 16:07:20 +0200

    modularizing main function

--- app/app.py
+++ app/app.py
@@ -74,45 +74,45 @@ df_d_csv = df_d10   # csv write df
 
 # ++++++++++++++++++++++ TRADING SETUP ++++++++++++++++++++++
 df_book = pd.DataFrame(columns= ["tradeType","buyPrice","qty","buyTime","sellPrice", "sellTime", "profit", "profit_per_second", "status"])
-funds = 100
-openTrade_ema = 0
-openTrade_macd = 0
-openTrade_so = 0
+
 trade_id_so = ""
 trade_id_macd = ""
 trade_id_ema = ""
 
+t0 = 0
 
-def udf_trade(openTrade, ttype, price, tradeid = ""):
+def udf_trade(ttype, price, wallet):
     global funds
-    global openTrade_so
+    
 
     if ttype == 1:
-        if funds == 0:
+        if wallet["funds"] == 0:
             print("ERROR: Not sufficient funds")
             return 0
         
-        if openTrade == 0 and ttype == 1:
+        if wallet["openTrade"] == 0 and ttype == 1:
             print(" *************** BUY ***************")
-            tradeid = str(secrets.token_hex(5))
+            wallet["trade_id"] = str(secrets.token_hex(5))
 
-            while tradeid in df_book.index:
-                tradeid = str(secrets.token_hex(5))
+            while wallet["trade_id"] in df_book.index:
+                wallet["trade_id"] = str(secrets.token_hex(5))
 
         else: 
             return print("invalid trade combination")
 
-        print(f"Trade ID found, starting Trade: {str(tradeid)}")
+        print(f"Trade ID found, starting Trade: {str(wallet['trade_id'])}")
 
         # assumes investment of full funds
-        df_book.loc[str(tradeid), ["tradeType", "buyPrice","qty", "buyTime", "status"]] = ["long",price, (funds/price), time.time(), "bought"]
+        df_book.loc[str(wallet["trade_id"]), ["tradeType", "buyPrice","qty", "buyTime", "status"]] = ["long",price, (wallet['funds']/price), time.time(), "bought"]
         print("Purchase Complete")
         print(df_book)
-        funds = 0
-        openTrade_so = 1
-        return tradeid
+        wallet["funds"] = 0
+        wallet['openTrade'] = 1
+        return wallet
 
-    if ttype == -1 and openTrade_so == 1:
+    if ttype == -1 and wallet["openTrade"] == 1:
+        tradeid = wallet['trade_id']
+
         print(f" *************** SELL: {tradeid} ***************")
         fProfit = df_book.loc[str(tradeid)]["qty"] *  (price - df_book.loc[str(tradeid)]["buyPrice"])
         fSalesDuration_s = time.time() - df_book.loc[str(tradeid)]["buyTime"]
@@ -127,11 +127,11 @@ def udf_trade(openTrade, ttype, price, tradeid = ""):
         print(f"==> Total Profit: {fProfit} in {fSalesDuration_s} seconds ||==> {(fProfit_per_sec):.3f} USD/sec")
         print("---------------------------------------\n")
 
-        openTrade_so = 0
-        funds = min(100, (df_book.loc[str(tradeid)]["qty"] * df_book.loc[str(tradeid)]["sellPrice"]))
-        tradeid = ""
-        print(f"New Funds: {funds}\n")
-        return tradeid
+        wallet["openTrade"] = 0
+        wallet["funds"] = min(100, (df_book.loc[str(tradeid)]["qty"] * df_book.loc[str(tradeid)]["sellPrice"]))
+        wallet["trade_id"] = ""
+        print(f"New Funds: {wallet['funds']}\n")
+        return wallet
 
 
 
@@ -174,166 +174,174 @@ def bin_ws(msg):
         df_rs = df_rs.iloc[0:0]
 
 
-
-
     
+def udf_startWS(vSymbol):
+    global r_int
+    global t0
+    print(f"Starting....{dt.datetime.now()}")
+    t0 = time.time()
 
-print(f"Starting....{dt.datetime.now()}")
-t0 = time.time()
+    bsm.start_symbol_ticker_socket(callback=bin_ws, symbol=vSymbol)
+    print(f"WS Start Time: {(time.time()-t0)}")
 
-bsm.start_symbol_ticker_socket(callback=bin_ws, symbol=symbol)
-print(f"WS Start Time: {(time.time()-t0)}")
-
-time.sleep(r_int) # crude approach, wait max r_int to get WS going
-
-# ++++++++++++++++++++++ ANALYSIS BLOCK ++++++++++++++++++++++
+    time.sleep(r_int) # crude approach, wait max r_int to get WS going
 
 
-# wait until sufficient data for rolling analysis 
-while len(df_d10) < iLookBack:
-    print(f"Min. Data Collection: {len(df_d10)} entries {(len(df_d10)/(iLookBack)):.0%} in {(time.time() - t0):.2f} seconds", end="\r")
-    time.sleep(1)
-print("\n Suffcient Data, lets START!!!")
-t_a1 = time.time()
 
-reqObs = 20
-print("")
-while True:
+def udf_tradeMASTER(vSymbol, vMethod):
+    """
+    Purpose: Master trading function. Split out to be accessed by different threads
+    Param: 
+    - vOpenTrade    => Open trade flag indicating active trade for method
+    - vSymbol       => Stock symbol to run trade for
+    - vMethod       => method of comparison, [so, ema, macd, ...] 
+    """
 
-    t_a2 = time.time()
-    
-    dfa = df_d10[["close"]].copy()  # analysis DF
-    
+    trade_id = ""
+    dWallet = {
+        "trade_id": ""
+        , "openTrade": 0
+        , "funds": 100
+    }
 
-    # ++++> EMA
-    wd_short = 5
-    wd_medium = 20
-    wd_medium_ema = 13
-    dfa["mav_short"] = dfa["close"].rolling(window=wd_short).mean()
-    dfa["mav_medium"] = dfa["close"].rolling(window=wd_medium).mean()
 
-    dfa["ema_short"] = dfa["close"].ewm(span=wd_short,  ignore_na=True).mean()
-    dfa["ema_med"] = dfa["close"].ewm(span=wd_medium_ema,  ignore_na=True).mean()
 
-        #... comp
+    udf_startWS(vSymbol)
 
-    ema_comp_cond=[
-        ((dfa["ema_short"].shift(1) < dfa["ema_med"].shift(1)) & (dfa["ema_short"] > dfa["ema_med"]))
-        , ((dfa["ema_short"].shift(1) > dfa["ema_med"].shift(1)) & (dfa["ema_short"] < dfa["ema_med"]))
-    ]
+    # wait until sufficient data for rolling analysis 
+    while len(df_d10) < iLookBack:
+        print(f"Min. Data Collection: {len(df_d10)} entries {(len(df_d10)/(iLookBack)):.0%} in {(time.time() - t0):.2f} seconds", end="\r")
+        time.sleep(1)
+    print("\n Suffcient Data, lets START!!!")
+    t_a1 = time.time()
 
-    ema_comp_choices =[1,-1]
-    
-    
 
-    # ++++> stochastic Oscilator
-    so_wd_k = 14
-    so_wd_d = 3
+    # ++++++++++++++++++++++ ANALYSIS BLOCK ++++++++++++++++++++++
 
-    dfa["k_line"] = ((dfa["close"] - dfa["close"].rolling(window=so_wd_k).min()) * 100 / 
-        (dfa["close"].rolling(window=so_wd_k).max() - dfa["close"].rolling(window=so_wd_k).min()))
+    print("")
 
-    dfa["d_line"] = dfa["k_line"].rolling(window=so_wd_d).mean()
+    ttrade_start = time.time()
 
-    dfa["so_20"] = 20
-    dfa["so_80"] = 80
+    while (time.time() - ttrade_start) < (5*60) or dWallet['openTrade'] == 1:
+
+        t_a2 = time.time()
+        
+        dfa = df_d10[["close"]].copy()  # analysis DF
+        dfa["tsv_so_k100"] = 0
+
+        # ++++> EMA
+        wd_short = 5
+        wd_medium = 20
+        wd_medium_ema = 13
+        dfa["mav_short"] = dfa["close"].rolling(window=wd_short).mean()
+        dfa["mav_medium"] = dfa["close"].rolling(window=wd_medium).mean()
+
+        dfa["ema_short"] = dfa["close"].ewm(span=wd_short,  ignore_na=True).mean()
+        dfa["ema_med"] = dfa["close"].ewm(span=wd_medium_ema,  ignore_na=True).mean()
 
             #... comp
 
+        ema_comp_cond=[
+            ((dfa["ema_short"].shift(1) < dfa["ema_med"].shift(1)) & (dfa["ema_short"] > dfa["ema_med"]))
+            , ((dfa["ema_short"].shift(1) > dfa["ema_med"].shift(1)) & (dfa["ema_short"] < dfa["ema_med"]))
+        ]
 
-    so_comp_cond=[
-        ((dfa["k_line"].shift(1) < 20) & (dfa["k_line"] > dfa["d_line"]))
-        , ((dfa["k_line"].shift(1) > 80) & (dfa["k_line"] < 80))
+        ema_comp_choices =[1,-1]
+        
+        
 
-    ]
+        # ++++> stochastic Oscilator
+        so_wd_k = 14
+        so_wd_d = 3
 
-    comp_choices =[1,-1]
+        dfa["k_line"] = ((dfa["close"] - dfa["close"].rolling(window=so_wd_k).min()) * 100 / 
+            (dfa["close"].rolling(window=so_wd_k).max() - dfa["close"].rolling(window=so_wd_k).min()))
 
-    k_last = dfa["k_line"].tail(1).item()
-    d_last = dfa["d_line"].tail(1).item() 
-    
-    # support variables. Set and maintain as soon as condition detected. Reset on sell
-    tsv_so_k100 = 0
-    tsv_so_Dmax = 0
-    tsv_so_k100 = 1 if k_last >= 95 and tsv_so_k100 == 0 else 0      # trade support variable: kmax 
-    tsv_so_Dmax = 1 if k_last >= 95 and tsv_so_Dmax == 0 else 0      # trade support variable: Dmax 
-    tsv_so_KD = 0                                                    # trade support variable: K falls below D
+        dfa["d_line"] = dfa["k_line"].rolling(window=so_wd_d).mean()
 
-    # if k and d have maxed, and k starts to fall below d
-    if tsv_so_KD < 2 and (tsv_so_Dmax + tsv_so_k100) == 2 and k_last < d_last:
-        tsv_so_KD += 1
-    
-    dfa["tsv_so_k100"] = tsv_so_k100
-    dfa["so_tsv_k100"] = tsv_so_Dmax
-    dfa["tsv_so_KD"] = tsv_so_KD
+        dfa["so_20"] = 20
+        dfa["so_80"] = 80
 
-    # ++++> MACD
-    macd_fast = 26
-    macd_slow = 12
-    macd_smooth = 9
-
-    dfa["macd_ema_fast"] = dfa["close"].ewm(span=macd_fast, adjust=False, ignore_na=True, min_periods=macd_fast).mean()
-    dfa["macd_ema_slow"] = dfa["close"].ewm(span=macd_slow, adjust=False, ignore_na=True, min_periods=macd_fast).mean()
-    dfa["macd_base"] = dfa["macd_ema_slow"] - dfa["macd_ema_fast"]
-    dfa["macd_signal"] = dfa["macd_base"].ewm(span=macd_smooth, adjust=False, ignore_na=True).mean()
-    dfa["macd_hist"] = dfa["macd_base"] - dfa["macd_signal"]
-    dfa["macd_color"] = np.where(dfa["macd_hist"]<0, "red", "green")
-
-        #... comp
-    macd_comp_cond=[
-        ((dfa["macd_base"].shift(1) < dfa["macd_signal"].shift(1)) & (dfa["macd_base"] > dfa["macd_signal"]))
-        , ((dfa["macd_base"].shift(1) > dfa["macd_signal"].shift(1)) & (dfa["macd_base"] < dfa["macd_signal"]))
-    ]
-
-    macd_comp_choices =[1,-1]
-    
-
-    # ++++> BOHLINGER
-    dfa["bohlinger_upper"] = dfa["mav_medium"] + (2*(dfa["mav_medium"].rolling(window=wd_medium).std()))
-    dfa["bohlinger_lower"] = dfa["mav_medium"] - (2*(dfa["mav_medium"].rolling(window=wd_medium).std()))
+                #... comp
 
 
-    #... OVERLL comp
-    dfa["ema_switch"] = np.select(ema_comp_cond, ema_comp_choices, default=0)
-    dfa["so_switch"] = np.select(so_comp_cond, comp_choices, default=0)
-    dfa["macd_switch"] = np.select(macd_comp_cond, macd_comp_choices, default=0)
+        so_comp_cond=[
+            ((dfa["k_line"].shift(1) < 20) & (dfa["k_line"] > dfa["d_line"]))
+            , ((dfa["k_line"].shift(1) > 80) & (dfa["k_line"] < 80))
+
+        ]
+
+        comp_choices =[1,-1]
 
 
-    # ++++++++++++++++++++++ TRADING ++++++++++++++++++++++
-    if dfa["so_switch"].tail(1).item() == 1 and openTrade_so == 0:
-        print("\BUY signal found")
-        print(dfa.tail(1))
+        # ++++> MACD
+        macd_fast = 26
+        macd_slow = 12
+        macd_smooth = 9
 
-        trade_id_so = udf_trade(openTrade_so, dfa["so_switch"].tail(1).item(), dfa["close"].tail(1).item(), trade_id_so)
+        dfa["macd_ema_fast"] = dfa["close"].ewm(span=macd_fast, adjust=False, ignore_na=True, min_periods=macd_fast).mean()
+        dfa["macd_ema_slow"] = dfa["close"].ewm(span=macd_slow, adjust=False, ignore_na=True, min_periods=macd_fast).mean()
+        dfa["macd_base"] = dfa["macd_ema_slow"] - dfa["macd_ema_fast"]
+        dfa["macd_signal"] = dfa["macd_base"].ewm(span=macd_smooth, adjust=False, ignore_na=True).mean()
+        dfa["macd_hist"] = dfa["macd_base"] - dfa["macd_signal"]
+        dfa["macd_color"] = np.where(dfa["macd_hist"]<0, "red", "green")
 
-        print(f"[[ buy complete: {openTrade_so} > {trade_id_so}]]")
-        time.sleep(5)
+            #... comp
+        macd_comp_cond=[
+            ((dfa["macd_base"].shift(1) < dfa["macd_signal"].shift(1)) & (dfa["macd_base"] > dfa["macd_signal"]))
+            , ((dfa["macd_base"].shift(1) > dfa["macd_signal"].shift(1)) & (dfa["macd_base"] < dfa["macd_signal"]))
+        ]
 
-    elif dfa["so_switch"].tail(1).item() == 1 and openTrade_so == 1:
-        print("\nTrade already open...")
-    
-    elif  dfa["so_switch"].tail(1).item() == -1 and openTrade_so == 1:
-        print("\nSELL signal found!")
-        print(dfa.tail(1))
-        trade_id_so = udf_trade(openTrade_so, dfa["so_switch"].tail(1).item(), dfa["close"].tail(1).item(), trade_id_so)
-        print(f"[[ SELL complete: {openTrade_so} > {trade_id_so}]]")
-        break
+        macd_comp_choices =[1,-1]
+        
 
-    # no need for other variables, as tsv_so_KD already includes conditions on k100 and Dmax
-    if tsv_so_KD == 2:
-        print("\nSELL signal: tsv_so_KD found!")
-        print(dfa.tail(1))
-        trade_id_so = udf_trade(openTrade_so, dfa["so_switch"].tail(1).item(), dfa["close"].tail(1).item(), trade_id_so)
-        print(f"[[ SELL complete: {openTrade_so} > {trade_id_so}]]")
-        break
+        # ++++> BOHLINGER
+        dfa["bohlinger_upper"] = dfa["mav_medium"] + (2*(dfa["mav_medium"].rolling(window=wd_medium).std()))
+        dfa["bohlinger_lower"] = dfa["mav_medium"] - (2*(dfa["mav_medium"].rolling(window=wd_medium).std()))
 
-    
-    # udf_progressBar(i+1,r_int*reqObs,reqObs, t_a1)
-    
-    time.sleep(1)
 
-print("\n")
-dfa.to_csv("./binance_dfa.csv", index=True)    
+        #... OVERLL comp
+        dfa["ema_switch"] = np.select(ema_comp_cond, ema_comp_choices, default=0)
+        dfa["so_switch"] = np.select(so_comp_cond, comp_choices, default=0)
+        dfa["macd_switch"] = np.select(macd_comp_cond, macd_comp_choices, default=0)
+
+
+        # ++++++++++++++++++++++ TRADING ++++++++++++++++++++++
+        if dfa[f"{vMethod}_switch"].tail(1).item() == 1 and dWallet["openTrade"] == 0:
+            print("\nBUY signal found")
+
+            dWallet = udf_trade(dfa[f"{vMethod}_switch"].tail(1).item(), dfa["close"].tail(1).item(), dWallet)
+
+            print(f"[[ buy complete: {dWallet['openTrade']} > {dWallet['trade_id']}]]")
+            time.sleep(5)
+
+        elif dfa[f"{vMethod}_switch"].tail(1).item() == 1 and dWallet["openTrade"] == 1:
+            print(".", end="")
+        
+        elif  dfa[f"{vMethod}_switch"].tail(1).item() == -1 and dWallet["openTrade"] == 1:
+            print("\nSELL signal found!")
+            dWallet = udf_trade(dfa[f"{vMethod}_switch"].tail(1).item(), dfa["close"].tail(1).item(), dWallet)
+            print(f"[[ SELL complete: {dWallet['openTrade']} > {dWallet['trade_id']}]]")
+
+        
+        time.sleep(1)
+
+    return dfa
+
+
+
+for m in ["so"]:
+    df_analysis = udf_tradeMASTER(symbol, vMethod=m)
+
+    udf_df_con(df_data)
+    print("\n ++++++++++++++++++++++++ DONE ++++++++++++++++++++++++")
+
+    cols = df_d10.select_dtypes(exclude=["datetime64"]).columns
+    df_d10[cols] = df_d10[cols].apply(pd.to_numeric, downcast="float",errors="coerce")
+
+    df_d10.to_csv(f"./df_interval_{m}.csv", index=True)
+    df_book.to_csv(f"./df_books_{m}.csv", index=True)
+    df_analysis.to_csv(f"./df_analysis_{m}.csv", index=True)    
 
 # ++++++++++++++++++++++ WS CLEAN UP ++++++++++++++++++++++
 bsm.stop()
@@ -345,13 +353,6 @@ print("Stopped....")
 
 
 # ++++++++++++++++++++++ FINAL DATA PROCESSING ++++++++++++++++++++++
-udf_df_con(df_data)
 
-cols = df_d10.select_dtypes(exclude=["datetime64"]).columns
-df_d10[cols] = df_d10[cols].apply(pd.to_numeric, downcast="float",errors="coerce")
-
-df_d_csv.to_csv("./binance_dfS.csv", index=True)
-df_d10.to_csv("./binance_df10.csv", index=True)
-df_book.to_csv("./df_books.csv", index=True)
 
 print("Le Fin")
